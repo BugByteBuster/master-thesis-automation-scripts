b0b6d67df778f8ceff3cfd003e1f0c808b22b4b9
vidyadharreddy18 <vidyadharlippy10@gmail.com>
2018-05-24 18:50:51 +0200

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -59,7 +59,7 @@ done
 #deleting endpoint group
 for endpoint in $(neutron vpn-endpoint-group-list | tail -n +4 | head -n -1 | awk '{print $2}')
 do
-neutron vpn-service-delete $endpoint 
+neutron vpn-endpoint-group-delete $endpoint 
 done
 
 
