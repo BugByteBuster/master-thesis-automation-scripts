d60404c2498c57fbb664066c9de2c9edd899b131
vidyadharreddy18 <vidyadharlippy10@gmail.com>
2018-06-15 15:42:51 +0200

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -47,4 +47,50 @@ for i in range(147, 150):
 
 	else:
 		stdin, stdout, stderr = ssh.exec_command('(cd /home/ubuntu/devstack; source openrc admin demo; ./delete.sh)')
+		
+		
+		
+************************************introduced threads**************************************************************
+#!/usr/bin/python
+import paramiko
+import time
+import threading
+threads = []
+def delete(i):
+    password = paramiko.RSAKey.from_private_key_file("/home/ubuntu/scripts/keys/my_key")
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    connection = ssh.connect(hostname="10.2.10." + str(i), username="ubuntu", pkey=password)
+    ssh.exec_command('sudo reboot')
+    time.sleep(4)
+    status = ssh.get_transport().is_active()
+    ssh.close()
+    print status
+    if not status:
+        j = 0
+        while j < 30:
+            try:
+                ssh.connect(hostname="10.2.10." + str(i), username="ubuntu", pkey=password)
+                if ssh.get_transport().is_active():
+                    print "executing at remote host"
+                    stdin, stdout, stderr = ssh.exec_command('(cd /home/ubuntu/devstack; source openrc admin demo; cd /home/ubuntu/tests; ./VPNaas_delete.sh)')
+                    print "stderr: ", stderr.readlines()
+                    print "pwd: ", stdout.readlines()
+                    break
+            except (paramiko.ssh_exception.NoValidConnectionsError, paramiko.ssh_exception.SSHException) as e:
+                continue
+            j = j + 1
+            time.sleep(5)
+
+    else:
+        stdin, stdout, stderr = ssh.exec_command(
+            '(cd /home/ubuntu/devstack; source openrc admin demo; cd /home/ubuntu/tests; ./VPNaas_delete.sh)')
+
+
+
+for i in range(146, 162):
+    t = threading.Thread(target=delete, args=(i,))
+    threads.append(t)
+    t.start()
+
 
