b3e985364b6edc6e3ee6eab964f4ac6f54319d02
vidyadhar <vidyadhar.bth@gmail.com>
2023-06-25 00:51:54 -0400

    Update serach_thread_analysis.py

--- serach_thread_analysis.py
+++ serach_thread_analysis.py
@@ -1,74 +1,48 @@
-"""
-import re
-import numpy as np
-import xlsxwriter
-
-
-workbook = xlsxwriter.Workbook('thread_results.xlsx')
-worksheet = workbook.add_worksheet()
-bold = workbook.add_format({'bold': 1})
-worksheet.write('A1', 'time taken to create extra routers', bold)
-
-def input(string, row, col):
-    with open("results.txt") as f:
-        array=[]
-        for line in f:
-            if str(string) in line:
-                x = re.split(string, line)
-                array.append((x[1]))
-        a = list(map(str.strip, array))
-        # print (a)
-        #a = sorted(a, key=lambda x: float(x))
-        for item in a:
-            worksheet.write(row, col, item)
-            row +=1
-input("time taken to create extra routers", 1, 0)
-workbook.close()
-"""
-from openpyxl import *
-import xlsxwriter
 import re
 import linecache
-line1="No of threads spawned 27"
-line2="time taken to create extra routers"
+import xlsxwriter
 
+def extract_lines(filename, output_filename, start_line, end_line):
+    with open(filename) as f1, open(output_filename, 'a') as f2:
+        for i, line in enumerate(f1):
+            if start_line in str(line):
+                for j in range(i+1, i+end_line+1):
+                    f2.write(linecache.getline(filename, j))
+        f2.close()
+        f1.close()
 
-workbook = xlsxwriter.Workbook('thread_results.xlsx')
-worksheet = workbook.add_worksheet()
-bold = workbook.add_format({'bold': 1})
-worksheet.write('A1', 'time taken to create extra routers', bold)
-
-with open('results.txt') as f1, open('results2.txt', 'a') as f2:
-    for i, line in enumerate(f1):
-        if line1 in str(line):
-            for j in range(i+1, i+194):
-                f2.write(linecache.getline('results.txt', j))
-    f2.close()
-    f1.close()
-
-with open('results2.txt') as f3:
-        row=1
-        col=0
+def write_data_to_excel(input_file, output_file, search_line, column_name):
+    workbook = xlsxwriter.Workbook(output_file)
+    worksheet = workbook.add_worksheet()
+    bold = workbook.add_format({'bold': 1})
+    worksheet.write('A1', column_name, bold)
+    
+    with open(input_file) as f:
+        row = 1
+        col = 0
         array = []
-        for f3_line in f3:
-            if line2 in f3_line:
-                x = re.split(line2, f3_line)
+        for line in f:
+            if search_line in line:
+                x = re.split(search_line, line)
                 array.append(x[1])
-                print(len(array))
                 if len(array) == 27:
-                    print("hitted")
-                    print(array)
                     a = list(map(str.strip, array))
                     for item in a:
-                        print(row, col)
                         worksheet.write(row, col, item)
                         row += 1
-                    row = row + 1
-
+                    row += 1
                     array = []
-        f3.close()
+        f.close()
 
-workbook.close()
+    workbook.close()
 
+# Specify the input and output file names
+input_filename = 'results.txt'
+output_filename = 'results2.txt'
+excel_filename = 'thread_results.xlsx'
 
+# Extract the desired lines from the input file
+extract_lines(input_filename, output_filename, "No of threads spawned 27", 194)
 
+# Write the extracted data to an Excel file
+write_data_to_excel(output_filename, excel_filename, "time taken to create extra routers", "time taken to create extra routers")
