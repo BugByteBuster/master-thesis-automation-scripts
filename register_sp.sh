f72dc4f808f889ad11e4ffd264f2cf60107b5827
vidyadhar <vidyadhar.bth@gmail.com>
2023-06-25 00:48:37 -0400

    Update register_sp.sh

--- register_sp.sh
+++ register_sp.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-for ip in `seq 1 10`;
-do
-  echo  ./add_sp.sh "sp-10.10.5.$ip" "10.10.5.$ip"
-  ./add_sp.sh "sp-10.10.5.$ip" "10.10.5.$ip"
+
+add_sp() {
+  local sp_name=$1
+  local sp_ip=$2
+  echo "./add_sp.sh \"$sp_name\" \"$sp_ip\""
+  ./add_sp.sh "$sp_name" "$sp_ip"
+}
+
+for ip in {1..10}; do
+  sp_name="sp-10.10.5.$ip"
+  sp_ip="10.10.5.$ip"
+  add_sp "$sp_name" "$sp_ip"
 done
