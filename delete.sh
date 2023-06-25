70b1c209e1e8b44caefc01b4d65c83b7ae93e18d
vidyadhar <vidyadhar.bth@gmail.com>
2023-06-25 00:48:10 -0400

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -1,72 +1,70 @@
 #!/bin/bash
 
-openstack router list
-nova list --all-tenants
+delete_vms() {
+  echo "Deleting VMs..."
+  nova list --all-tenants | awk 'NR>3 && NF {print $2}' | xargs -I {} nova delete {}
+}
 
-#deleting vm
-for vm in $(nova list --all-tenants | tail -n +4 | head -n -1 | awk '{print$2}')
-do
-    nova delete $vm
-done
+delete_s2s_connections() {
+  echo "Deleting S2S connections..."
+  neutron ipsec-site-connection-list | awk 'NR>3 && NF {print $2}' | xargs -I {} neutron ipsec-site-connection-delete {}
+}
 
-#deleting s2s connection
-for s2s in $(neutron ipsec-site-connection-list | tail -n+4 | head -n -1 |awk '{print $2}')
-do
-neutron ipsec-site-connection-delete $s2s
-done
+delete_ike_policies() {
+  echo "Deleting IKE policies..."
+  neutron vpn-ikepolicy-list | awk 'NR>3 && NF {print $2}' | xargs -I {} neutron vpn-ikepolicy-delete {}
+}
 
-#deleting ike
-for ike in $(neutron vpn-ikepolicy-list | tail -n +4 | head -n -1 | awk '{print $2}')
-do
-neutron vpn-ikepolicy-delete $ike 
-done
+delete_endpoint_groups() {
+  echo "Deleting endpoint groups..."
+  neutron vpn-endpoint-group-list | awk 'NR>3 && NF {print $2}' | xargs -I {} neutron vpn-endpoint-group-delete {}
+}
 
-#deleting endpoint group
-for endpoint in $(neutron vpn-endpoint-group-list | tail -n +4 | head -n -1 | awk '{print $2}')
-do
-neutron vpn-endpoint-group-delete $endpoint 
-done
+delete_vpn_services() {
+  echo "Deleting VPN services..."
+  neutron vpn-service-list | awk 'NR>3 && NF {print $2}' | xargs -I {} neutron vpn-service-delete {}
+}
 
-#deleting vpnservice
-for service in $(neutron vpn-service-list | tail -n +4 | head -n -1 | awk '{print $2}')
-do
-neutron vpn-service-delete $service 
-done
+delete_ipsec_policies() {
+  echo "Deleting IPsec policies..."
+  neutron vpn-ipsecpolicy-list | awk 'NR>3 && NF {print $2}' | xargs -I {} neutron vpn-ipsecpolicy-delete {}
+}
 
+delete_images() {
+  echo "Deleting images..."
+  openstack image list | awk 'NR>3 && NF {print $2}' | xargs -I {} openstack image delete {}
+}
 
-#deleting isec
-for ipsec in $(neutron vpn-ipsecpolicy-list | tail -n +4 | head -n -1 | awk '{print $2}')
-do
-neutron vpn-ipsecpolicy-delete $ipsec 
-done
+delete_routers_and_ports() {
+  echo "Deleting routers and router ports..."
+  openstack router list | awk '/router_/ {print $2}' | while read -r router; do
+    openstack router show "$router" >/dev/null
+    if [ $? -eq 0 ]; then
+      openstack router delete "$router"
+      openstack router show "$router"
+      openstack subnet list | awk '/subnet_/ {print $2}' | while read -r subnet; do
+        openstack router remove subnet "$router" "$subnet"
+        openstack router delete "$router"
+      done
+    fi
+  done
+}
 
-#deleting image
-for image in $(openstack image list | tail -n +4 | head -n -1 | awk '{print $2}')
-do 
-    openstack image delete $image
-done
+delete_networks() {
+  echo "Deleting networks..."
+  openstack network list | awk '/net/ {print $2}' | xargs -I {} openstack network delete {}
+}
 
-
-#deleting router and router ports
-for router in $(openstack router list | egrep "router_" | awk '{print $2}')
-do
-    openstack router delete $router
-    openstack router show $router
-    for subnet in $(openstack subnet list | grep  "subnet_" | awk '{print $2}')
-    do
-        openstack router remove subnet $router $subnet
-        openstack router delete $router
-    done
-done
-
-
-#deleting network
-for network in $(openstack network list | tail -n +4 |head -n -1 | grep "net" | awk '{print$2}')
-do
-   openstack network delete $network
-done
-
-
+echo "Executing cleanup..."
+delete_vms
+delete_s2s_connections
+delete_ike_policies
+delete_endpoint_groups
+delete_vpn_services
+delete_ipsec_policies
+delete_images
+delete_routers_and_ports
+delete_networks
 
 openstack network list
 openstack router list
@@ -75,5 +73,5 @@ openstack router list
 openstack image list
 neutron vpn-ipsecpolicy-list
 neutron vpn-ikepolicy-list
-neutron vpn-service-list 
+neutron vpn-service-list
 neutron vpn-endpoint-group-list
