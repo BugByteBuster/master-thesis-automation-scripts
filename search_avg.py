d823812376fe8dc53c3990e152083113fa84b3eb
vidyadhar <vidyadhar.bth@gmail.com>
2023-06-25 00:50:52 -0400

    Update search_avg.py

--- search_avg.py
+++ search_avg.py
@@ -1,16 +1,28 @@
 import re
 import statistics as s
-values=[]
 import numpy as np
-#numbers=[]
-with open('results.txt') as file:
-    for line in file:
-            if "7. Time taken to create s2s connectivity" in line:
-                x=re.split("7. Time taken to create s2s connectivity", line)
+
+def extract_data(file_path, keyword):
+    values = []
+    with open(file_path) as file:
+        for line in file:
+            if keyword in line:
+                x = re.split(keyword, line)
                 values.append(x[1])
-    a=list(map(str.strip, values))
-    #numbers = [int(float(x)) for x in a]
-    x = np.array(a)
+    return list(map(str.strip, values))
+
+def calculate_mean(data):
+    x = np.array(data)
     y = x.astype(np.float)
-    print (y)
-    print (np.mean(y))
+    return np.mean(y)
+
+# Define the file path and keyword
+file_path = 'results.txt'
+keyword = "7. Time taken to create s2s connectivity"
+
+# Extract the data and calculate the mean
+data = extract_data(file_path, keyword)
+mean = calculate_mean(data)
+
+# Print the mean value
+print(mean)
